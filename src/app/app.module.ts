e182d8a22b2d15aae299c7a4266e5e7f0f5569df
Aditya R. Jariwala <ajariwala@indy.bhsolutions.com>
2018-05-21 10:05:57 -0400

    Header Started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LangPanelComponent } from './lang-panel/lang-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LangPanelComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/lang-panel/lang-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lang-panel',
+  templateUrl: './lang-panel.component.html',
+  styleUrls: ['./lang-panel.component.css']
+})
+export class LangPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
